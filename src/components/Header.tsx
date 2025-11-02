461013deec5da754967625e6158b204cd01351e6
varshith pilli <varshith6946@gmail.com>
2025-11-02 13:44:41 +0530

    Initial gh-pages commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Header() {
+  return (
+    <header className="mb-10 text-center">
+      <h1 className="text-4xl font-semibold text-white asset-regular">Bars</h1>
+      <p className="text-gray-400 mt-2 text-md tinos-regular">A collection of words that linger</p>
+    </header>
+  );
+}
---
+++ src/data/lyrics.ts
@@ -0,0 +1,83 @@
+export interface LyricItem {
+  lyric: string;
+  song: string;
+  artist: string;
+}
+
+export const lyrics: LyricItem[] = [
+  {
+    lyric: `Richard don't make watches for presidents \n Just a million trapped between skeletons`,
+    song: "Chains & Whips",
+    artist: "Clipse, Kendrick Lamar",
+  },
+  {
+    lyric: `If I had her, then you had her, she never mattered`,
+    song: "So Be it",
+    artist: "Clipse",
+  },
+  {
+    lyric: `The good news is nigga you came a long way \n The bad news is nigga you went the wrong way`,
+    song: "Love Yourz",
+    artist: "J. Cole",
+  },
+  {
+    lyric: `Uncle said, "Nigga, you must be sick \n All you talk about jus just gettin' rich"`,
+    song: "Chains & Whips",
+    artist: "Clipse, Kendrick Lamar",
+  },
+  {
+    lyric: `Mama told me the power was in the tongue \n But it probably ain't powerful as a gun`,
+    song: "Fuel",
+    artist: "Eminem, JID",
+  },
+  {
+    lyric: `Ain't nobody but the mirror lookin' for the fall off`,
+    song: "Count Me Out",
+    artist: "Kendrick Lamar",
+  },
+  {
+    lyric: `I had a dream I could buy my way to Heaven \n When I awoke, I spent that on a necklace`,
+    song: "Can't Tell Me Nothing",
+    artist: "Kanye West",
+  },
+  {
+    lyric: `And I wonder if you know \n What it means to find your dreams`,
+    song: "I Wonder",
+    artist: "Kanye West",
+  },
+  {
+    lyric: `Do you even remember what the issue is? \n You just tryna find where the tissue is`,
+    song: "I Wonder",
+    artist: "Kanye West",
+  },
+  {
+    lyric: `You say I think I'm never wrong \n You know what? Maybe you're right`,
+    song: "I Wonder",
+    artist: "Kanye West",
+  },
+  {
+    lyric: `True friends, one question \n Bitch, where you when I was walkin'? \n Now I run the game, got the whole world talkin' \n King Kunta, everybody wanna cut the legs off him`,
+    song: "King Kunta",
+    artist: "Kendrick Lamar",
+  },
+  {
+    lyric: `And if I gotta brown-nose for some gold \n Then I'd rather be a bum than a motherfuckin' baller`,
+    song: "King Kunta",
+    artist: "Kendrick Lamar",
+  },
+  {
+    lyric: `Chivalry ain't dead, you ain't let her go alone`,
+    song: "The Birds Don't Sing",
+    artist: "Clipse, John Legend",
+  },
+  {
+    lyric: `The way you missed Mama, I guess I should've known \n Chivalry ain't dead, you ain't let her go alone`,
+    song: "The Birds Don't Sing",
+    artist: "Clipse, John Legend",
+  },
+  {
+    lyric: `The way you spelled it out, there's an L in every lesson \n "Boy, you owe it to the world, let your mess become your message"`,
+    song: "The Birds Don't Sing",
+    artist: "Clipse, John Legend",
+  },
+];
